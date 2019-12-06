d6030612aa45bcafeed71b9ac8633d828fabe0a8
juanagustin0504 <juanagustin@naver.com>
2019-12-06 13:27:53 +0900

    First Commit

---
+++ Anonymous Diary/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Anonymous Diary
+//
+//  Created by Webcash on 2019/12/06.
+//  Copyright © 2019 Moon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Anonymous Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anonymous Diary
+//
+//  Created by Webcash on 2019/12/06.
+//  Copyright © 2019 Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
